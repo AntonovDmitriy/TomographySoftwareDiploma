641cf9a8e8c4bfa0f41185b4011b87846f27c75b
Antonov Dmitry <antonov.other@gmail.com>
2014-02-19 21:41:44 +0400

    error profiles

--- src/main/java/com/antonov/tomographysoftwarediploma/impl/Tomograph.java
+++ src/main/java/com/antonov/tomographysoftwarediploma/impl/Tomograph.java
@@ -28,25 +28,25 @@ import org.slf4j.LoggerFactory;
  * @author Antonov
  */
 public class Tomograph {
-    
+
     private static Logger logger = LoggerFactory.getLogger(Tomograph.class);
     ModellingModuleController modellingModuleController;
     HardwareModuleController hardwareModuleController;
-    
+
     private static final String TOMOGRAPH_CONF_PATH = "conf/tomograph.conf";
     private static ResourceBundle bundle;
     private Properties tomographProperty = new Properties();
-    
+
     public ModellingModule modellingModule;
     public HardwareModule hardwareModule;
-    
+
     public Set<String> setFilterName;
     public Set<PInterpolation> setInterpolation;
-    
+
     public StartModeEnum startMode;
-    
+
     private NotAvaliableStartModuleException startModuleException;
-    
+
     private void removeTemporaryFiles() throws IOException {
         logger.info("removing temporary files");
         if (startMode.equals(Tomograph.StartModeEnum.MODE_WEBSTART)
@@ -57,11 +57,11 @@ public class Tomograph {
             }
         }
     }
-    
+
     public static enum StartModeEnum {
-        
+
         MODE_WEBSTART, MODE_STANDALONE, MODE_INSTALLED, NONE;
-        
+
         @Override
         public String toString() {
             switch (this) {
@@ -78,9 +78,9 @@ public class Tomograph {
             }
         }
     }
-    
+
     public Tomograph() {
-        
+
         setStartModeOfApplicaton();
         initTomographProperty();
         downloadMainJar();
@@ -91,21 +91,21 @@ public class Tomograph {
         this.modellingModule = new ModellingModule(this, tomographProperty);
         this.hardwareModule = new HardwareModule(this, tomographProperty, startModuleException);
     }
-    
+
     public void setModellingController(ModellingModuleController controller) {
         this.modellingModuleController = controller;
         this.modellingModule.setController(controller);
     }
-    
+
     public void setHardwareController(HardwareModuleController controller) {
         this.hardwareModuleController = controller;
     }
-    
+
     private void initTomographProperty() {
         try (InputStream is = new FileInputStream(TOMOGRAPH_CONF_PATH)) {
             tomographProperty.load(is);
             logger.info("Successfully loading config file " + TOMOGRAPH_CONF_PATH);
-            
+
         } catch (IOException ex) {
             try (InputStream ist = getClass().getClassLoader().getResourceAsStream(TOMOGRAPH_CONF_PATH)) {
                 tomographProperty.load(ist);
@@ -115,9 +115,9 @@ public class Tomograph {
             }
         }
     }
-    
+
     private void downloadMainJar() {
-        
+
         try {
             if (startMode.equals(StartModeEnum.MODE_WEBSTART)) {
                 ReaderWriterData.downloadFileAndWriteToTempFolder(tomographProperty.getProperty("URL_TO_MAIN_JAR"), tomographProperty.getProperty("PATH_TO_MAIN_JAR"));
@@ -127,10 +127,10 @@ public class Tomograph {
             exitApplication();
         }
     }
-    
+
     private void setStartModeOfApplicaton() {
         try {
-            
+
             ReaderWriterData reader = new ReaderWriterData();
             String manifest = reader.getStringResource("META-INF/MANIFEST.MF");
             if (manifest.contains("Start_mode: " + StartModeEnum.MODE_WEBSTART)) {
@@ -149,12 +149,12 @@ public class Tomograph {
             this.startMode = StartModeEnum.NONE;
         }
     }
-    
+
     public void prepareViews() {
         modellingModule.prepareView();
         hardwareModule.prepareView();
     }
-    
+
     public void exitApplication() {
         logger.info("Closing application");
         try {
@@ -166,21 +166,22 @@ public class Tomograph {
             }
             removeTemporaryFiles();
             logger.info("=======Stop TomographySoftware 1.0.0 application=======");
-            System.exit(0);//cierra aplicacion
         } catch (Exception ex) {
             logger.error("Error while closing application", ex);
+        } finally {
+            System.exit(0);
         }
     }
-    
+
     void showViewer(BufferedImage image) {
         AppLaunch.showImageViewer(image);
     }
-    
+
     void showDensityAnalizator(BufferedImage image) {
-        
+
         AppLaunch.showDensityAnalizator(image);
     }
-    
+
     public void initFilterNames() {
         setFilterName = new TreeSet<>();
         setFilterName.add("ramp");
@@ -190,23 +191,23 @@ public class Tomograph {
         setFilterName.add("blackman");
         setFilterName.add("none");
     }
-    
+
     public void initInterpolations() {
-        
+
         setInterpolation = new HashSet<>();
         PInterpolation pojo = new PInterpolation();
         pojo.setValue(SinogramCreator.REGIME_LINEAR_ITERPOLATION);
         pojo.setNameInteprolation(bundle.getString("LINEAR_INTERPOLATION"));
         setInterpolation.add(pojo);
-        
+
         pojo = new PInterpolation();
         pojo.setValue(SinogramCreator.REGIME_NEAREST_NEIGHBOUR_INTERPOLATION);
         pojo.setNameInteprolation(bundle.getString("NEAREST_NEIGHBOUR_INTERPOLATION"));
         setInterpolation.add(pojo);
     }
-    
+
     public boolean isColoringEnumContainsColorName(String coloringFromProperty) {
-        
+
         for (ColorFunctionNamesEnum color : ColorFunctionNamesEnum.values()) {
             if (color.toString().equals(coloringFromProperty)) {
                 return true;
@@ -214,14 +215,14 @@ public class Tomograph {
         }
         return false;
     }
-    
+
     private void initRequiredSshConnectionFilesToSystem() {
         try {
             String javaHome = System.getProperty("java.home");
             logger.info("Java home: " + javaHome);
             String javaSecurityFolder = javaHome + "/lib/security";
             logger.info("security folder: " + javaSecurityFolder);
-            
+
             if (ReaderWriterData.isFolderExists(javaSecurityFolder)) {
                 String javaVersion = System.getProperty("java.version");
                 logger.info("java version: " + javaVersion);
@@ -236,9 +237,9 @@ public class Tomograph {
             startModuleException = new NotAvaliableStartModuleException(bundle.getString("ERROR_START_CONNECTION_FILES"));
         }
     }
-    
+
     private void copySecurityFiles(String javaVersion, String javaSecurityFolder) throws IOException {
-        
+
         switch (startMode) {
             case MODE_INSTALLED: {
                 if (javaVersion.equals("7")) {
@@ -265,10 +266,10 @@ public class Tomograph {
                 } else if (javaVersion.equals("6")) {
                     copySecurityFilesFromTargetDirectory("target/classes/connectionFiles/jce6", javaSecurityFolder);
                 }
-            
+
         }
     }
-    
+
     private void copySecurityFilesFromJar(String nameOfFolder, String folderDestination) throws UnsupportedEncodingException, IOException {
         ReaderWriterData reader = new ReaderWriterData();
         String pathOfJar = "";
@@ -282,12 +283,12 @@ public class Tomograph {
         }
         reader.copyJarFolder(pathOfJar, nameOfFolder, folderDestination);
     }
-    
+
     private void copySecurityFilesFromTargetDirectory(String nameOfFolder, String folderDestination) throws IOException {
-        
+
         ReaderWriterData.copyFilesFromTo(nameOfFolder, folderDestination);
     }
-    
+
     private void initBundle() {
         try {
             bundle = ResourceBundle.getBundle("conf/bundle");
@@ -301,5 +302,5 @@ public class Tomograph {
             }
         }
     }
-    
+
 }
